1639fe17cdd74714fd83b7c383a0f82adcd6a9b3
Anton Chestikov <Anton.auc@gmail.com>
2019-12-22 23:01:54 +0500

    Added camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _targetTransform;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = _targetTransform.position - transform.position;
+    }
+
+    private void Update()
+    {
+        FollowTargetHorizontally();
+    }
+
+    private void FollowTargetHorizontally()
+    {
+        Vector3 newPosition = _targetTransform.position - _offset;
+
+        transform.position = new Vector3(newPosition.x, transform.position.y, newPosition.z);
+    }
+}

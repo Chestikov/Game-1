7a791a685999623db195e5996b3a20a0ab783cad
Anton Chestikov <Anton.auc@gmail.com>
2019-12-22 17:53:57 +0500

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private int GameSceneIndex;
+
+    public void PlayGame() => SceneManager.LoadScene(GameSceneIndex);
+
+    public void QuitGame() => Application.Quit();
+}

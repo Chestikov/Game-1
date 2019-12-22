fa2cd11052b9737e236b716fd6684829263e0356
Anton Chestikov <Anton.auc@gmail.com>
2019-12-22 21:37:20 +0500

    Added coin script

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int _value = 1;
+    public int Value
+    {
+        get
+        {
+            return _value;
+        }
+    }
+}
